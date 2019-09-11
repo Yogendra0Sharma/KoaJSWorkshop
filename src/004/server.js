95ed0fd877428ef2892d5838bf4474effdd4145a
Yogendra Sharma <eh.yogendra@gmail.com>
2019-09-11 22:13:13 +0530

    add root router using koa-router & register routes middleware

--- src/004/server.js
+++ src/004/server.js
@@ -1,7 +1,16 @@
 const Koa = require('koa');
 // import koa-router
+const Router = require('koa-router');
 const app = new Koa();
+const router = new Router();
 const PORT = 4000;
+
+// Create a root ('/') router & send 'Welcome to Koa' message to client.
+router.get('/',ctx => {
+    ctx.body = "Welcome to Koa";
+})
+// register routes middleware to app
+app.use(router.routes());
 
 app.listen(PORT,function(){
     console.log(`Server is running on ${PORT}`);
