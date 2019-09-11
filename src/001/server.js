2bedb92d1cc7d926fc1f722195fffceec87ac8d7
Yogendra Sharma <eh.yogendra@gmail.com>
2019-09-11 21:30:34 +0530

    Add await to next() method

--- src/001/server.js
+++ src/001/server.js
@@ -1,6 +1,11 @@
 const Koa = require('koa');
 const app = new Koa();
 const PORT = 4000;
+
+// Add Middleware
+app.use(async ctx => {
+    ctx.body = "Hello World";
+})
 app.listen(PORT,function(){
     console.log(`Server is running on ${PORT}`);
 });
