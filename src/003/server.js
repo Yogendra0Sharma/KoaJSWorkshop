7142b348b0db034641af91ff29114cfe68e357f6
Yogendra Sharma <eh.yogendra@gmail.com>
2019-09-11 21:58:25 +0530

    add Koa context request and response method

--- src/003/server.js
+++ src/003/server.js
@@ -1,13 +1,18 @@
 // Context Object
+// Koa context encapsulate request & response objects
 const Koa = require('koa');
 const app = new Koa();
 const PORT = 4000;
 
 app.use(async ctx => {
     // we can get request and response methods from context object.
+    // request object methods
     console.log(ctx.querystring);
     console.log(ctx.ip);
     ctx.body = "Hello World";
+    // response object methods
+    ctx.status = 400;
+    // goto browser network console tab and check status
 })
 
 app.listen(PORT,function(){
