77c2e9b410a31be4ce323ca6d93ac33e4d4a36ce
Yogendra Sharma <eh.yogendra@gmail.com>
2019-09-11 23:31:10 +0530

    Add superheros endpoint to get all heros

--- src/004/server.js
+++ src/004/server.js
@@ -5,9 +5,29 @@ const app = new Koa();
 const router = new Router();
 const PORT = 4000;
 
-// Create a root ('/') router & send 'Welcome to Koa' message to client.
+// Create a root ('/') router with GET request & send 'Welcome to Koa' message to client.
 router.get('/',ctx => {
     ctx.body = "Welcome to Koa";
+})
+
+// Create a GET request for '/superheors' endpoint and return all super heros name
+const heros = [
+    {
+        "id" : 01,
+        "name" : "Thor"
+    },
+    {
+        "id" : 02,
+        "name" : "Iron Man"
+    },
+    {
+        "id" : 03,
+        "name" : "Hulk"
+    }
+];
+
+router.get('/superheros', ctx => {
+    ctx.body = heros;
 })
 // register routes middleware to app
 app.use(router.routes());
