3b2860daf7c9404d60d2eccacaa9b29621476bad
Yogendra Sharma <eh.yogendra@gmail.com>
2019-09-11 21:23:52 +0530

    Add Middleware to log method and url

---
+++ src/002/server.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+const app = new Koa();
+const PORT = 4000;
+
+// Add Middleware
+app.use(async (ctx,next) => {
+    console.log(`${ctx.method} ${ctx.url} ${new Date()}`);
+    next();
+})
+app.use(async ctx => {
+    ctx.body = "Hello World";
+})
+app.listen(PORT,function(){
+    console.log(`Server is running on ${PORT}`);
+});
